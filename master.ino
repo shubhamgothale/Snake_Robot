7c4233fdeec11bd8589b5fe7fee71d355f4f6174
shubhamgothale <shubhamiitkgp2503@gmail.com>
2018-08-11 00:08:24 +0530

    Initial Commit

---
+++ master.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+float amplitude;
+float angle;
+float frequency;
+int on=0;
+float d;
+
+byte y;
+byte x;
+byte f;
+byte k;
+
+
+
+
+void setup() { Wire.begin();
+Serial.begin(9600);
+digitalWrite(2, HIGH);}
+
+void loop()
+{
+ on=digitalRead(4);
+
+ amplitude = analogRead(A0); 
+ amplitude = map(amplitude, 1, 1023, 0, 8);
+ frequency = analogRead(A1);
+ frequency = map(frequency, 1, 1023, 1, 10);
+ angle= (57.2957795130* acos(amplitude/10));
+ float n;
+ float g;
+ float ff;
+ for(n=angle;n<=90;n++)
+    {
+      ff= n*(90-angle)/90;
+      g = g+(12-(10.24*sin(ff*0.0174532925)));
+    }
+    d =frequency*(1997.22/(4*g));
+    g=0;
+ 
+ x=on;
+ y=angle;
+ f= frequency;
+ k=d;
+ Serial.println(f);
+ Serial.println(amplitude);
+ delay(1000);
+ Wire.beginTransmission(7);
+ Wire.write(f);        
+ Wire.write(y);
+ Wire.write(x);
+ Wire.write(k);
+ Wire.endTransmission(); 
+}
+
---
+++ slave.ino
@@ -0,0 +1,72 @@
+#include <Wire.h>
+#include <Servo.h>
+Servo myservo;
+float pos = 0;
+float pp;
+int in;
+float m;
+
+float t;
+int z=0;
+float q=0;
+float h=0;
+float d=2;
+void setup()
+{
+  Serial.begin(9600);
+  myservo.attach(9);
+  Wire.begin(7);                
+  Wire.onReceive(e);
+  for (in = 10; in <= 90; in += 1)
+  {
+   myservo.write(in);           
+   delay(20);                       
+  }
+  delay(500);
+
+}
+
+void loop() 
+{
+    for (pos = q; pos <= h; pos += 1)
+    {
+      myservo.write(pos);
+      pp=map(pos,q,h,0,180);     
+      m = 12-(10.24*sin(0.0174532925*pp));
+      delay((d*m)+1);
+      Serial.print("position is ");
+      Serial.print(pos);
+      Serial.print("\t delay is ");
+      Serial.println(24/(m*d));     
+    }
+    for (pos =h; pos >=q; pos -= 1) 
+    {
+      myservo.write(pos);
+      pp=map(pos,q,h,0,180);          
+      m = 12-(10.24*sin(0.0174532925*pp));
+      delay((d*m)+1);
+      Serial.print("position is  ");
+      Serial.print(pos);
+      Serial.print("\t  delay is ");
+      Serial.println(24/(m*d));
+       
+    }
+}
+
+void e(int b)
+{
+  float f = Wire.read();
+  float y = Wire.read();
+  int x = Wire.read();
+  float k = Wire.read();
+  t=1997.22*f;
+  delay(15);
+  z=x;
+  if(z==1){
+  delay(t/4);
+  q=y;
+  h=(180-y);
+  
+  d=k;
+  }
+}
